92f64e2d3f0d3f59609648563f87578819182118
zb1391 <zmb1391@gmail.com>
2015-12-12 22:19:43 -0500

    setting up named routes with a versioning system. also copied some tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
-Rails.application.routes.draw do
-  namespace :api do
+require 'api_constraints'
 
+Rails.application.routes.draw do
+  namespace :api, defaults: { format: :json }  do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+
+    end
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
