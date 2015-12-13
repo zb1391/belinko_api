81f46b223a9b14b36a924b489d862eb5961b2574
zb1391 <zmb1391@gmail.com>
2015-12-13 17:29:20 -0500

    added a create endpoint for users

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -18,4 +18,45 @@ describe Api::V1::UsersController do
       expect(response.status).to eql 200
     end
   end
+
+  describe "POST #create" do
+
+    context "when is successfully created" do
+      before(:each) do
+        @user_attributes = FactoryGirl.attributes_for :user
+        post :create, { user: @user_attributes }, format: :json
+      end
+
+      it "renders the json representation for the user record just created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql @user_attributes[:email]
+      end
+
+      it "responds with a 201" do
+        expect(response.status).to eql 201
+      end
+    end
+
+    context "when is not created" do
+      before(:each) do
+        @invalid_user_attributes = { password: "12345678",
+                                     password_confirmation: "12345678" }
+        post :create, { user: @invalid_user_attributes }, format: :json
+      end
+      
+      it "renders an errors json" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it "renders the json errors on why the user could not be created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "can't be blank"
+      end
+
+      it "responds with a 422" do
+        expect(response.status).to eql 422
+      end
+    end
+  end
 end
