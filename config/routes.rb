3ea23fbd4baae26cb2d0bb3e51bc77200c709885
zb1391 <zmb1391@gmail.com>
2015-12-13 17:12:37 -0500

    added a users controller and routes to get user

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api, defaults: { format: :json }  do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-
+      resources :users, only: [:show]
     end
   end
   # The priority is based upon order of creation: first created -> highest priority.
