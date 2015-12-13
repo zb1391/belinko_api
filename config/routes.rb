0d242268139bc1a1f099fa4c3cc3e5fb90abd0ed
zb1391 <zmb1391@gmail.com>
2015-12-12 22:27:13 -0500

    added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
+  devise_for :users
   namespace :api, defaults: { format: :json }  do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
 
