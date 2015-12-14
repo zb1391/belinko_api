d49e45d063f07d6ef09471dbb72d2b1301ea0b6d
zb1391 <zmb1391@gmail.com>
2015-12-13 22:23:39 -0500

    refactored the omniauth_callbacks_controller spec to work

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151214005001) do
+ActiveRecord::Schema.define(version: 20151214024431) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -28,8 +28,10 @@ ActiveRecord::Schema.define(version: 20151214005001) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
+    t.string   "auth_token",             default: ""
   end
 
+  add_index "users", ["auth_token"], name: "index_users_on_auth_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["provider"], name: "index_users_on_provider"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
--- spec/controllers/api/v1/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/api/v1/omniauth_callbacks_controller_spec.rb
@@ -1,15 +1,5 @@
 require 'spec_helper'
 
-# set OmniAuth in test mode
-OmniAuth.config.test_mode = true
-
-OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
-  :provider => 'facebook',
-  :uid => '123545',
-  :info => {
-    :email => 'test@test.com'
-  }
-})
 
 describe Api::V1::OmniauthCallbacksController do
   before(:each) { request.headers['Accept'] = "application/vnd.belinko.v1" }
@@ -17,7 +7,9 @@ describe Api::V1::OmniauthCallbacksController do
   describe "POST #facebook" do
     before(:each) do
       request.env["devise.mapping"] = Devise.mappings[:user]
-      request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
+      auth = OmniAuth.config.mock_auth[:facebook]
+      auth.credentials = { :token => (User.count+1).to_s+"-token" }
+      request.env["omniauth.auth"] = auth
       post :facebook, format: :json 
     end
 
