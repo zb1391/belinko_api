2f5b960e551de39b315a7ae42cd82cbfbe1c8a3c
zb1391 <zmb1391@gmail.com>
2015-12-30 10:03:48 -0500

    created the add_reviews method for GooglePlace. Right now it gets all reviews from our database for a given place and appends it to the google_resp json

--- lib/google_places/google_place.rb
+++ lib/google_places/google_place.rb
@@ -4,13 +4,23 @@ module GooglePlaces
 
     def initialize(options={})
       @google_resp = options[:google_resp]
-      
-      @place = Place.find_or_create_by(gid: @google_resp[:place_id]) do |place|
-        place.name      = @google_resp[:name]
-        place.latitude  = @google_resp[:geometry][:location][:lat]
-        place.longitude = @google_resp[:geometry][:location][:lng]
+       
+      @place = Place.find_or_create_by(gid: @google_resp["place_id"]) do |place|
+        place.name      = @google_resp["name"]
+        place.latitude  = @google_resp["geometry"]["location"]["lat"]
+        place.longitude = @google_resp["geometry"]["location"]["lng"]
       end
+
+      add_reviews
     end
 
+    
+    private
+    def add_reviews
+      @google_resp["reviews"] = []
+      @place.reviews.each do |review|
+        @google_resp["reviews"] << review.as_json
+      end
+    end
   end
 end
--- spec/helper_modules/google_places_spec.rb
+++ spec/helper_modules/google_places_spec.rb
@@ -28,12 +28,12 @@ describe GooglePlaces::GooglePlace do
   before(:each) do
     place = Place.first.nil? ? FactoryGirl.create(:place) : Place.first
     @google_resp = {
-      place_id: place.gid,
-      name:     place.name,
-      geometry: {
-        location: {
-          lat: 123,
-          lng: 102,
+      "place_id" => place.gid,
+      "name"     => place.name,
+      "geometry" => {
+        "location" => {
+          "lat" => 123,
+          "lng" => 102,
         },
       },
     }
@@ -53,13 +53,29 @@ describe GooglePlaces::GooglePlace do
     describe "when the place does not yet exist" do
       before(:each) do
         place = Place.last
-        @google_resp[:place_id] = place.gid+1
+        @google_resp["place_id"] = "#{place.gid.to_i+1}"
       end
 
       it "creates a new place" do
         count = Place.count
         GooglePlaces::GooglePlace.new(google_resp: @google_resp)
         expect(Place.count).to eql(count+1)
+      end
+    end
+  end
+
+  describe "#add_reviews" do
+    before(:each) do
+      @place = FactoryGirl.create(:place, gid: "#{Place.count+1}")
+      3.times { FactoryGirl.create(:review, place: @place) }
+      @google_resp["place_id"] = @place.gid
+    end
+
+    it "adds reviews to the google_resp" do
+      google_place = GooglePlaces::GooglePlace.new(google_resp: @google_resp)
+      google_place.send :add_reviews
+      @place.reviews.each do |review|
+        expect(@google_resp["reviews"]).to include(review.as_json)
       end
     end
   end
