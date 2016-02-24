4d1d86636e0e39515f68578fb05eba7653cd579e
zb1391 <zmb1391@gmail.com>
2016-02-23 21:49:54 -0500

    did some stupid stuff that doesnt matter to the omniauth_callbacks_controller. we are not using it anymore anyways

--- app/controllers/api/v1/omniauth_callbacks_controller.rb
+++ app/controllers/api/v1/omniauth_callbacks_controller.rb
@@ -2,12 +2,9 @@ class Api::V1::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     # finds the user or creates one
     @user = User.from_omniauth(request.env["omniauth.auth"])
-
-
     if @user.persisted?
       @user.update_auth_token(request.env["omniauth.auth"].credentials.token)
       sign_in @user, event: :authentication
- 
       # return the omni-auth data in the response
       # including the user's uid and the auth-token
       render json: request.env['omniauth.auth'], status: 201 
