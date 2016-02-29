1703090982d38c463bc3131940ac792472ed2e51
zb1391 <zmb1391@gmail.com>
2016-02-28 20:01:52 -0500

    cleaned up the google places controller

--- app/controllers/api/v1/google_places_controller.rb
+++ app/controllers/api/v1/google_places_controller.rb
@@ -8,29 +8,6 @@ class Api::V1::GooglePlacesController < ApplicationController
   respond_to :json
   helper_method :missing_params
   
-  def test
-    render json: {test: true}, status: 200
-  end
-
-  def login_test
-    begin
-      user_creds = Omniauth::Facebook.authenticate(params[:code])
-      # finds the user or creates one
-      @user = User.from_omniauth(user_creds)
-      if @user.persisted?
-        @user.update_auth_token(user_creds["token"])
-        sign_in @user, event: :authentication
-        # return the omni-auth data in the response
-        # including the user's uid and the auth-token
-        render json: user_creds, status: 201
-      else
-        render json: {errors: {facebook: "Failed to log in"}}, status: 422
-      end
-    rescue
-      render json: {errors: {facebook: "Failed to log in"}}, status: 422
-    end
-  end
-
   def radar_search
     res = GooglePlacesApi.search!("radar",params)
     render json: res.json_response, status: 200
